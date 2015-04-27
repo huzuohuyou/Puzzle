1426ba3a256221f6344c531c472d7b0363202c97
huzuohuyou <1255246777@qq.com>
2015-04-27 17:47:05 +0800

    1.0.0.1
    first commit

---
+++ Puzzle/Puzzle.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Puzzle
+{
+    partial class Puzzle
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lab_time = new System.Windows.Forms.Label();
+            this.btn_import = new System.Windows.Forms.Button();
+            this.btn_sta = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackColor = System.Drawing.Color.MediumPurple;
+            this.pictureBox9.Location = new System.Drawing.Point(200, 200);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox9.TabIndex = 0;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox9.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox9.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.pictureBox8.Location = new System.Drawing.Point(100, 200);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox8.TabIndex = 0;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox8.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox8.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.DarkViolet;
+            this.pictureBox7.Location = new System.Drawing.Point(0, 200);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox7.TabIndex = 0;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox7.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox7.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Sienna;
+            this.pictureBox6.Location = new System.Drawing.Point(200, 100);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox6.TabIndex = 0;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox6.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox6.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Tomato;
+            this.pictureBox5.Location = new System.Drawing.Point(100, 100);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox5.TabIndex = 0;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox5.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox5.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Firebrick;
+            this.pictureBox4.Location = new System.Drawing.Point(0, 100);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.pictureBox3.Location = new System.Drawing.Point(200, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.pictureBox2.Location = new System.Drawing.Point(100, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Teal;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Location = new System.Drawing.Point(405, 61);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(75, 23);
+            this.btn_reset.TabIndex = 4;
+            this.btn_reset.Text = "重置";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(464, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "秒";
+            // 
+            // lab_time
+            // 
+            this.lab_time.AutoSize = true;
+            this.lab_time.Location = new System.Drawing.Point(405, 141);
+            this.lab_time.Name = "lab_time";
+            this.lab_time.Size = new System.Drawing.Size(29, 12);
+            this.lab_time.TabIndex = 2;
+            this.lab_time.Text = "0000";
+            // 
+            // btn_import
+            // 
+            this.btn_import.Location = new System.Drawing.Point(405, 101);
+            this.btn_import.Name = "btn_import";
+            this.btn_import.Size = new System.Drawing.Size(75, 23);
+            this.btn_import.TabIndex = 1;
+            this.btn_import.Text = "导入图片";
+            this.btn_import.UseVisualStyleBackColor = true;
+            // 
+            // btn_sta
+            // 
+            this.btn_sta.Location = new System.Drawing.Point(406, 22);
+            this.btn_sta.Name = "btn_sta";
+            this.btn_sta.Size = new System.Drawing.Size(75, 23);
+            this.btn_sta.TabIndex = 0;
+            this.btn_sta.Text = "开始";
+            this.btn_sta.UseVisualStyleBackColor = true;
+            this.btn_sta.Click += new System.EventHandler(this.btn_sta_Click);
+            // 
+            // Puzzle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 302);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lab_time);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.btn_import);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_sta);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Name = "Puzzle";
+            this.Text = "拼图";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lab_time;
+        private System.Windows.Forms.Button btn_import;
+        private System.Windows.Forms.Button btn_sta;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Puzzle/Puzzle.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Puzzle
+{
+    public partial class Puzzle : Form
+    {
+        //图片位置字典
+        Dictionary<PictureBox, Point> pictureLocationDict = new Dictionary<PictureBox, Point>();
+
+        //所拖拽的图片
+        PictureBox currentPictureBox = null;
+        //被迫需要移动的图片
+        PictureBox haveToPictureBox = null;
+        //原位置
+        Point oldLocation = Point.Empty;
+        //新位置
+        Point newLocation = Point.Empty;
+        //鼠标按下坐标（control控件的相对坐标） 
+        Point mouseDownPoint = Point.Empty;
+        //显示拖动效果的矩形 
+        Rectangle rect = Rectangle.Empty;
+        //是否正在拖拽 
+        bool isDrag = false;
+
+        public Puzzle()
+        {
+            InitializeComponent();
+            pictureLocationDict.Add(pictureBox1, pictureBox1.Location);
+            pictureLocationDict.Add(pictureBox2, pictureBox2.Location);
+            pictureLocationDict.Add(pictureBox3, pictureBox3.Location);
+            pictureLocationDict.Add(pictureBox4, pictureBox4.Location);
+            pictureLocationDict.Add(pictureBox5, pictureBox5.Location);
+            pictureLocationDict.Add(pictureBox6, pictureBox6.Location);
+            pictureLocationDict.Add(pictureBox7, pictureBox7.Location);
+            pictureLocationDict.Add(pictureBox8, pictureBox8.Location);
+            pictureLocationDict.Add(pictureBox9, pictureBox9.Location);
+        }
+
+        private void Puzzle_Paint(object sender, PaintEventArgs e)
+        {
+            if (rect != Rectangle.Empty)
+            {
+                if (isDrag)
+                {
+                    e.Graphics.DrawRectangle(Pens.White, rect);
+                }
+                else
+                {
+                    e.Graphics.DrawRectangle(new Pen(this.BackColor), rect);
+                }
+            }
+        }
+
+     
+        /// <summary>
+        /// 不好用
+        /// </summary>
+        /// <returns></returns>
+        public PictureBox GetPictureBoxByLocation()
+        {
+            PictureBox pic = null;
+            if (this.ActiveControl.Name.Contains("pictureBox"))
+            {
+                pic = (PictureBox)this.ActiveControl;
+            }
+            return pic;
+
+        }
+
+        public PictureBox GetPictureBoxByLocation(MouseEventArgs e)
+        {
+            PictureBox pic = null;
+            foreach (PictureBox item in pictureLocationDict.Keys)
+            {
+                if (e.Location.X > item.Location.X && e.Location.Y > item.Location.Y && item.Location.X + 100 > e.Location.X && item.Location.Y + 100 > e.Location.X)
+                {
+                    pic = item;
+                }
+            }
+            return pic;
+        }
+
+        public PictureBox GetPictureBoxByLocation(int x,int y)
+        {
+            PictureBox pic = null;
+            foreach (PictureBox item in pictureLocationDict.Keys)
+            {
+                if (x> item.Location.X && y > item.Location.Y && item.Location.X + 100 > x && item.Location.Y + 100 > y)
+                {
+                    pic = item;
+                }
+            }
+            return pic;
+        }
+
+        /// <summary>
+        /// 通过hashcode获取picture，用mouseeventargs之后获取相对于picture的坐标不是相对窗体
+        /// </summary>
+        /// <param name="hascode"></param>
+        /// <returns></returns>
+        public PictureBox GetPictureBoxByHashCode(string hascode)
+        {
+            PictureBox pic = null;
+            foreach (PictureBox item in pictureLocationDict.Keys)
+            {
+                if (hascode == item.GetHashCode().ToString())
+                {
+                    pic = item;
+                }
+            }
+            return pic;
+        }
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            oldLocation = new Point(e.X, e.Y);
+            currentPictureBox = GetPictureBoxByHashCode(sender.GetHashCode().ToString());
+            MoseDown(currentPictureBox, sender, e);
+        }
+
+        public void MoseDown(PictureBox pic, object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                oldLocation = e.Location;
+                rect = pic.Bounds;
+            }
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrag = true;
+                rect.Location = getPointToForm(new Point(e.Location.X - oldLocation.X, e.Location.Y - oldLocation.Y));
+                this.Refresh();
+
+            }
+        }
+
+       
+
+        private void reset()
+        {
+            mouseDownPoint = Point.Empty;
+            rect = Rectangle.Empty;
+            isDrag = false;
+        }
+
+        private Point getPointToForm(Point p)
+        {
+            return this.PointToClient(pictureBox1.PointToScreen(p));
+        }
+
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            oldLocation = new Point(currentPictureBox.Location.X, currentPictureBox.Location.Y);
+            if (oldLocation.X + e.X > 300 || oldLocation.Y + e.Y > 300||oldLocation.X + e.X < 0 || oldLocation.Y + e.Y < 0)
+            {
+                return;
+            }
+            haveToPictureBox = GetPictureBoxByLocation(oldLocation.X + e.X, oldLocation.Y + e.Y);
+            newLocation = new Point(haveToPictureBox.Location.X, haveToPictureBox.Location.Y);
+            haveToPictureBox.Location = oldLocation;
+            PictureMouseUp(currentPictureBox, sender, e);
+        }
+
+        public void PictureMouseUp(PictureBox pic, object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (isDrag)
+                {
+                    isDrag = false;
+                    pic.Location = newLocation;
+                    this.Refresh();
+                }
+                reset();
+            }
+        }
+
+        public void ExchangePictureBox(MouseEventArgs e)
+        { }
+
+        private void btn_sta_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this.ActiveControl.Name);
+        }
+
+
+
+    }
+}
